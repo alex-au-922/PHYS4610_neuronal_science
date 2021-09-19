3f238c967d80bc2363c6f2e1f71ccc425f5c75a7
Au Cheuk Ming <alex@kali.kali>
2021-09-19 20:21:14 +0800

    Commit

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     #    Load constant values from .yaml
     w_matrix = None
     with open('constants.yaml') as stream:
-        network_constant = yaml.safe_load(stream)["NeuronNetwork"]
+        network_constant = yaml.safe_load(stream)["Main"]
     
     # 2. Create Neuron Network from weight matrix, u, v
     network = NeuronNetworkTimeSeries(w_matrix)
