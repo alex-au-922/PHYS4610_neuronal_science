cc83436f4261355a72b7b11076f7a46dde535067
Peter Tong <petertongkawai@gmail.com>
2021-09-19 18:21:34 +0800

    Initialization of Neuron Network

--- network/network.py
+++ network/network.py
@@ -1,7 +1,10 @@
 from .node import *
 
 class NeuronNetwork:
-    def __init__(self, w_matrix):
+    def __init__(self, w_matrix, init_u_arr, init_v_arr):
+        self.w_matrix = w_matrix
+        self.init_u_arr = init_u_arr
+        self.init_v_arr = init_v_arr
         self.node_list = []
         self.node_map = {}
         self.initialize_from_adj_matrix(w_matrix)
@@ -11,24 +14,26 @@ class NeuronNetwork:
         for column in w_matrix.T:
             assert all(column >= 0) or all(column <= 0)   
 
-    def initialize_from_adj_matrix(self, w_matrix):
-        (i_max, j_max) = w_matrix.shape
+    def initialize_from_adj_matrix(self):
+        (i_max, j_max) = self.w_matrix.shape
         for i in range(1, i_max):
+            self._create_node(i)
             for j in range(1, j_max):
-                if w_matrix[i][j] == 0:
-                    continue
-                if j not in self.node_map:
-                    if (w_matrix[i][j] > 0):
-                        temp = ExciNeuron()
-                        self.node_map[j] = temp
-                        self.node_list.append(temp)
-                    if (w_matrix[i][j] < 0):
-                        temp = InhiNeuron()
-                        self.node_map[j] = temp
-                        self.node_list.append(temp)
-                    continue
-                if j in self.node_map:
-                    pass
+                self._create_node(j)
+                w_ij = self.w_matrix[i][j]
+                if (w_ij != 0):
+                    self.node_map[j].direct_to(self.node_map[i], w_ij)
+
+    def _create_node(self, n):
+        '''Create Node n if not exists'''
+        if n not in self.node_map:
+            # Check if Node n is Excitor or Inhibitor
+            if any(self.w_matrix[i][n] > 0 for i in range(1, len(self.w_matrix))):
+                self.node_map[n] = ExciNeuron(self.init_u_arr[n], self.init_v_arr[n])
+            if any(self.w_matrix[i][n] < 0 for i in range(1, len(self.w_matrix))):
+                self.node_map[n] = InhiNeuron(self.init_u_arr[n], self.init_v_arr[n])
+            self.node_list.append(self.node_map[n])
+                    
 
 
                 
