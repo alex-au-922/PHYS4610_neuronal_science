329ef5e20cd2e14c60a98fc859ece5ec247c3507
Peter Tong <petertongkawai@gmail.com>
2021-09-19 19:11:51 +0800

    testing

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+from network.network import NeuronNetwork
 import utils
 import numpy as np
 
 def main():
     # 1. Load weight matrix from file
+    w_matrix = None
     # 2. Create Neuron Network from weight matrix, u, v
+    network = NeuronNetwork(w_matrix)
     # 3. Step until time t, store 
+    dt = 0.125
+    time = 0
+    
     pass
 
 
