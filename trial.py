07a5ae1fe5fb1afb29bbf6ca5d25424e0a4a94fe
alexau922 <alexuau922@gmail.com>
2021-09-26 16:50:13 +0800

    commit

--- trial.py
+++ trial.py
@@ -2,6 +2,7 @@ from utils.plot import PlotGraph
 import pathlib
 
 def main():
-    directory = pathlib('./result/')
+    directory = pathlib.Path('./result/2021-09-26 15:17:25.671920/')
     graph = PlotGraph(directory, 'log.csv')
-    
+if __name__ == "__main__":
+    main()
--- utils/plot.py
+++ utils/plot.py
@@ -1,16 +1,49 @@
 from os import pathconf
 import matplotlib.pyplot as plt
-from typing import List
+from typing import List, Dict
 import os
-import pandas as pd
+import csv
 import numpy as np
+import seaborn as sns
+from tqdm import tqdm
 
 class PlotGraph:
     def __init__(self, pathname, filename):
+        self.pathname = pathname
         print('Reading data...')
-        self.data = pd.read_csv(os.path.join(pathname, filename), header = None).values
-        print(self.data)
+        with open(os.path.join(pathname, filename), 'r') as file:
+            self.data = {}
+            reader = csv.reader(file)
+            for key, row in enumerate(tqdm(reader)):
+                buff_row = []
+                for value in row:
+                    buff_row.append(int(value))
+                self.data[key] = buff_row
+        self.plot_graphs()
+    
+    def plot_graphs(self):
+        self.firing_frequency_probability_distribution()
 
-    def firing_frequency_probability_distribution(data: List[List[int]], pathname: str):
-        pass
+    def write_csv(self, data: List[Tuple], filepath: str):
+        with open(filepath, 'w', newline = '') as file:
+            writer = csv.write(file)
+            writer.writerows(data)
+    
+    def firing_frequency_probability_distribution(self):
+        print('Calculating the firing distribution...')
+        index = []
+        length = []
+        for i,row in self.data.items():
+            index.append(i)
+            length.append(len(row) / 7500)
+        self.write_csv(zip(index, length), os.path.join(self.pathname, 'firing_rate.csv')
+        fig,ax = plt.subplots()
+        sns.kdeplot(x = index, y = length, ax = ax)
+        ax.set(xlabel = "Firing Rate (Hz)", ylabel = "Probability Density")
+        fig.savefig(os.path.join(self.pathname, 'firing_rate.jpg')
+
+
+
+
+        
 
