Fixed the bug of normalization

trial.py
@@ -1,8 +1,10 @@
-from utils.plot import PlotGraph
-import pathlib
+import numpy as np
+import matplotlib.pyplot as plt
 
-def main():
-    directory = pathlib.Path('./result/2021-09-27 22:52:01.068981/')
-    graph = PlotGraph(directory, 'log.csv')
-if __name__ == "__main__":
-    main()
+a = np.random.normal(loc= 0, scale = 3, size = 10000000)
+start_val = -5
+end_val = 5
+density, x_val = np.histogram(a, bins = np.linspace(start_val, end_val, 100), density= True)
+fig, ax = plt.subplots()
+ax.plot(x_val[:-1], density)
+plt.show()

utils/plot.py
@@ -55,10 +55,10 @@ class PlotGraph:
 
         length = np.array(length)
         density, x_value = np.histogram(length, bins = np.linspace(0, np.max(length), 100), density = True)
-        norm_density = density / density.sum()
+        x_value = (x_value[1:] + x_value[:-1])/2
 
         fig,ax = plt.subplots()
-        ax.plot(x_value[:-1], norm_density)
+        ax.plot(x_value, density)
         ax.set_xlim(0, None)
         ax.set_ylim(0, None)
         ax.set(xlabel = "Firing Rate (Hz)", ylabel = "Probability Density")
@@ -90,11 +90,11 @@ class PlotGraph:
 
         interval = np.log10(np.array(interval, dtype = np.float64)*self.arg['dt'] / 1000)
         density, x_value = np.histogram(interval, bins = np.linspace((np.min(interval)), np.max(interval), 80), density = True)
-        norm_density = density / density.sum()
         x_value = np.power(10,x_value)
+        x_value = (x_value[1:] + x_value[:-1])/2
 
         fig,ax = plt.subplots()
-        ax.semilogx(x_value[:-1],norm_density)
+        ax.semilogx(x_value,density)
         ax.set(xlabel = "ISI (s)", ylabel = "Probability Density")
         fig.savefig(os.path.join(self.pathname, 'log_spike_interval.jpg'))
 