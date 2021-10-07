35e898d1c5891f17f58c24e8a2b717ab117c6699
alexau922 <alexuau922@gmail.com>
2021-10-07 21:55:48 +0800

    Commit

---
+++ utils/parser.py
@@ -0,0 +1,9 @@
+import argparse
+
+def parse_arg():
+    parser = argparse.ArgumentParser('Specify the dt, totalTime and maxSpike')
+    parser.add_argument('-d', '--dt', default = 0.125, type = float, help = 'Time separations')
+    parser.add_argument('-t','--totalTime', default = 7500, type = int, help = 'Total time of experiment')
+    parser.add_argument('-m','--maxSpike', default = 1000, type = int, help = 'Maximum spike stored before truncation')
+    args = parser.parse_args()
+    return args
